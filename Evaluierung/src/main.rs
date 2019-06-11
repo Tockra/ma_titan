2f1313d1bdf8bfc32efc598d3ccdb53c7dede4f7
Tim <tim.tannert@tu-dortmund.de>
2019-06-11 20:26:11 +0200

    -Todo updated

--- Evaluierung/src/main.rs
+++ Evaluierung/src/main.rs
@@ -2,5 +2,5 @@ use std::mem;
 use std::collections::HashMap;
 
 fn main() {
-    println!("Hello, world! {}",mem::size_of::<Option<HashMap<u8,HashMap<u8,u32>>>>());
+    println!("Hello, world! {}",mem::size_of::<bool>());
 }
--- Implementierung/src/lib.rs
+++ Implementierung/src/lib.rs
@@ -12,15 +12,15 @@ pub trait PredecessorList<T> {
     fn maximum(&self) -> Option<T>; 
 }
 
-type Store = i32;
+pub type Store = i32;
 pub struct STree<Store> {
     pub minimum: Store,
     pub maximum: Store,
     pub count: u32,
     pub root_table: [FnvHashMap<u8,FnvHashMap<u8,u32>>;1 << (8 * mem::size_of::<i32>()/2)],
-    pub root_top: [bool; 1 << (8 * mem::size_of::<i32>()/2)],
-    pub l1_top: [bool; (1 << (8 * mem::size_of::<i32>()/2))/32],
-    pub l2_top: [bool; ((1 << (8 * mem::size_of::<i32>()/2))/32)/32],
+    pub root_top: [u8; 1 << (8 * mem::size_of::<i32>()/2)/8],
+    pub l1_top: [u8; (1 << (8 * mem::size_of::<i32>()/2))/32/8],
+    pub l2_top: [u8; ((1 << (8 * mem::size_of::<i32>()/2))/32)/32/8],
     pub element_list: LinkedList<Store>,
 }
 
@@ -30,4 +30,5 @@ pub struct STree<Store> {
 - prüfen wie die Datenstruktur im Vergleich zur Datenstruktur in C++ abschneidet
 - prüfen ob die 2Mbyte Initialisierungsspeicher durch Verzicht auf Pointer, einen Leistungsschub bringen 
 - herausfinden ob X86 CPUs 32-Bit oder 64-Bit Instruktionen zum Ermitteln von bestimmten Bits haben
+- u32 Bit je Zelle in root_top (weniger Top Arrays evtl. in der Praxis notwendig!)
 */
