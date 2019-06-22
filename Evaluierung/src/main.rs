9820cf1a5e92dae45ef05f98d772233556f6fa0c
Tim <tim.tannert@tu-dortmund.de>
2019-06-22 17:51:51 +0200

    ...

--- Evaluierung/src/main.rs
+++ Evaluierung/src/main.rs
@@ -1,6 +1,10 @@
-use std::mem;
-use std::collections::HashMap;
-
+#[cfg(target_arch = "x86")]
+use std::arch::x86::*;
+#[cfg(target_arch = "x86_64")]
+use std::arch::x86_64::*;
 fn main() {
-    println!("Hello, world! {}",mem::size_of::<bool>());
+    unsafe {
+        let all_bytes_zero = _mm256_setzero_si256();
+        println!("{:?}", all_bytes_zero);
+    }
 }
