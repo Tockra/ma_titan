6d07eb3b1101a848fe0a88b235f50ba0086c3a4c
Tim <tim.tannert@tu-dortmund.de>
2019-06-24 11:22:25 +0200

    - Modul sauber definiert - PredList-Trait angepasst (Methoden hinzugefügt) - Todo angepasst

--- Implementierung/src/internal.rs
+++ Implementierung/src/internal.rs
@@ -1,265 +1,272 @@
+use std::ptr;
+pub struct List<T> {
+    first: Option<Box<Element<T>>>,
+    last: *mut Element<T>,
+    len: usize,
+}
 
-mod internal {
-    use std::ptr;
-    pub struct List<T> {
-        first: Option<Box<Element<T>>>,
-        last: *mut Element<T>,
-        len: usize,
-    }
+pub struct Element<T> {
+    next: Option<Box<Element<T>>>,
+    prev: *mut Element<T>,
+    elem: T,
+}
 
-    pub struct Element<T> {
-        next: Option<Box<Element<T>>>,
-        prev: *mut Element<T>,
-        elem: T,
-    }
-
-    impl<T> List<T> {
-        pub fn new() -> Self {
-            List {
-                first: None,
-                last: ptr::null_mut(),
-                len: 0,
-            }
-        }
-
-        /* Fügt am Ende der Liste ein Element mit Wert 'elem' ein.*/
-        pub fn insert_at_end(&mut self, elem: T) {
-            if self.len() == 0 {
-                let mut node = Box::new(Element {
-                    next: None,
-                    prev: ptr::null_mut(),
-                    elem
-                });
-                self.last = &mut *node;
-                self.first = Some(node);
-            }
-            else {
-                let mut node = Box::new(Element {
-                    next: None,
-                    prev: self.last,
-                    elem
-                });
-
-                let tmp: *mut _ = &mut *node;
-                unsafe {
-                  (*(self.last)).next = Some(node);
-                }
-                self.last = tmp;
-            }
-            self.len += 1;
-        }
-
-        pub fn len(&self) -> usize {
-            self.len
-        }
-
-        pub fn pop_front(&mut self) -> Option<T> {
-            self.first.take().map(|head| {
-                let mut head = *head;
-                self.first = head.next;
-
-                if self.first.is_none() {
-                    self.last = ptr::null_mut();
-                }
-                head.prev =  ptr::null_mut();
-                head.elem
-            })
-        }
-
-        pub fn pop_back(&mut self) -> Option<T> {
-            if self.last.is_null() {
-                return None;
-            }
-
-            unsafe {
-                let node = Box::from_raw(self.last);
-                
-                if node.prev.is_null() {
-                    self.first = None;
-                } else {
-                    (*node.prev).next = None;
-                };
-                
-                self.last = node.prev;
-
-                Some(node.elem)
-            }
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            first: None,
+            last: ptr::null_mut(),
+            len: 0,
         }
     }
 
-    impl<T> Element<T> {
-        fn new(elem: T) -> Self {
-            Element {
+    /* Fügt am Ende der Liste ein Element mit Wert 'elem' ein.*/
+    pub fn insert_at_end(&mut self, elem: T) {
+        if self.len() == 0 {
+            let mut node = Box::new(Element {
                 next: None,
                 prev: ptr::null_mut(),
-                elem,
-            }
+                elem
+            });
+            self.last = &mut *node;
+            self.first = Some(node);
         }
+        else {
+            let mut node = Box::new(Element {
+                next: None,
+                prev: self.last,
+                elem
+            });
 
-        /* Hinter dem Element (self) wird das Element elem in die Liste eingefügt. Hier bei
-            muss beachtet werden, dass extern die Size angepasst werden muss und der Last-Zeiger evtl. angepasst werden muss. */
-        fn insert_after(&mut self, mut elem: Box<Element<T>>) {
-            elem.prev = &mut *self;
-            self.next = Some(elem);
-        }
-
-        /* Vor dem Element (self) wird das Element elem in die Liste eingefügt. Hier bei
-            muss beachtet werden, dass extern die Size angepasst werden muss und ggf. der First-Zeiger angepasst werden muss.  */
-        fn insert_before(mut self, elem: *mut Element<T>) {
+            let tmp: *mut _ = &mut *node;
             unsafe {
-                self.prev = &mut *elem;
-                (*elem).next = Some(Box::new(self));
+                (*(self.last)).next = Some(node);
             }
+            self.last = tmp;
+        }
+        self.increase_count();
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.first.take().map(|head| {
+            let mut head = *head;
+            self.first = head.next;
+
+            if self.first.is_none() {
+                self.last = ptr::null_mut();
+            }
+            head.prev =  ptr::null_mut();
+            self.decrease_count();
+            head.elem
+        })
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        if self.last.is_null() {
+            return None;
+        }
+
+        unsafe {
+            let node = Box::from_raw(self.last);
+            
+            if node.prev.is_null() {
+                self.first = None;
+            } else {
+                (*node.prev).next = None;
+            };
+            
+            self.last = node.prev;
+            self.decrease_count();
+            Some(node.elem)
         }
     }
 
-   // #[cfg(test)]
-    pub mod tests {
-        #[test]
-        pub fn test_insert_and_pop() {
-            let mut l = super::List::new();
-            fill_list(&mut l);
-            assert_eq!(l.len(), 6);
+    #[inline]
+    fn increase_count(&mut self) {
+        self.len += 1;
+    }
 
-            let len = l.len();
-            assert_eq!(l.pop_front().unwrap(), -20);
-            assert_eq!(l.pop_front().unwrap(), 10);
-            assert_eq!(l.pop_front().unwrap(), 20);
-            assert_eq!(l.pop_front().unwrap(), 30);
-            assert_eq!(l.pop_front().unwrap(), 40);
-            assert_eq!(l.pop_front().unwrap(), 50);
-
-            fill_list(&mut l);
-            
-            assert_eq!(l.pop_back().unwrap(), 50);
-            assert_eq!(l.pop_back().unwrap(), 40);
-            assert_eq!(l.pop_back().unwrap(), 30);
-            assert_eq!(l.pop_back().unwrap(), 20);
-            assert_eq!(l.pop_back().unwrap(), 10);
-            assert_eq!(l.pop_back().unwrap(), 0);
-
-            fill_list(&mut l);
-
-            assert_eq!(l.pop_front().unwrap(), -20);
-            assert_eq!(l.pop_back().unwrap(), 50);
-            assert_eq!(l.pop_front().unwrap(), 10);
-            assert_eq!(l.pop_back().unwrap(), 40);
-            assert_eq!(l.pop_front().unwrap(), 20);
-            assert_eq!(l.pop_back().unwrap(), 30);
+    #[inline]
+    fn decrease_count(&mut self) {
+        if self.len == 0 {
+            panic!("Die Länge einer internal::List kann nicht 0 unterschreiten!");
         }
-
-        #[inline]
-        fn fill_list(l: &mut super::List<i32>) {
-            l.insert_at_end(-20);
-            l.insert_at_end(10);
-            l.insert_at_end(20);
-            l.insert_at_end(30);
-            l.insert_at_end(40);
-            l.insert_at_end(50);
-        }
-
-        #[test]
-        pub fn test_insert_before_first() {
-            let mut l = super::List::new();
-            fill_list(&mut l);
-            let mut elem = Box::new(super::Element::new(23));
-            l.first.unwrap().insert_before(&mut *elem);
-            // Das kann die Methode aus technischen Gründen nicht übernehmen und muss extern gemacht werden!
-            l.first = Some(elem);
-
-            assert_eq!(l.pop_front().unwrap(), 23);
-            assert_eq!(l.pop_front().unwrap(), -20);
-            assert_eq!(l.pop_front().unwrap(), 10);
-            assert_eq!(l.pop_front().unwrap(), 20);
-            assert_eq!(l.pop_front().unwrap(), 30);
-            assert_eq!(l.pop_front().unwrap(), 40);
-            assert_eq!(l.pop_front().unwrap(), 50);
-
-            fill_list(&mut l);
-            let mut elem = Box::new(super::Element::new(23));
-            l.first.unwrap().insert_before(&mut *elem);
-            // Das kann die Methode aus technischen Gründen nicht übernehmen und muss extern gemacht werden!
-            l.first = Some(elem);
-            assert_eq!(l.pop_back().unwrap(), 50);
-            assert_eq!(l.pop_back().unwrap(), 40);
-            assert_eq!(l.pop_back().unwrap(), 30);
-            assert_eq!(l.pop_back().unwrap(), 20);
-            assert_eq!(l.pop_back().unwrap(), 10);
-            assert_eq!(l.pop_back().unwrap(), -20);
-            assert_eq!(l.pop_back().unwrap(), 23);
-
-            fill_list(&mut l);
-            let mut elem = Box::new(super::Element::new(23));
-            l.first.unwrap().insert_before(&mut *elem);
-            // Das kann die Methode aus technischen Gründen nicht übernehmen und muss extern gemacht werden!
-            l.first = Some(elem);
-            assert_eq!(l.pop_front().unwrap(), 23);
-            assert_eq!(l.pop_back().unwrap(), 50);
-            assert_eq!(l.pop_back().unwrap(), 40);
-            assert_eq!(l.pop_back().unwrap(), 30);
-            assert_eq!(l.pop_back().unwrap(), 20);
-            assert_eq!(l.pop_back().unwrap(), 10);
-            assert_eq!(l.pop_back().unwrap(), -20);
-            
-            
-        }
-
-        #[test]
-        pub fn test_insert_after_last() {
-            let mut l = super::List::new();
-            fill_list(&mut l);
-            let mut elem = super::Element::new(23);
-            let tmp = l.last;
-            l.last = &mut elem;
-            unsafe {
-                (*tmp).insert_after(Box::new(elem));
-            }
-
-            // Das kann die Methode aus technischen Gründen nicht übernehmen und muss extern gemacht werden!
-
-            assert_eq!(l.pop_front().unwrap(), -20);
-            assert_eq!(l.pop_front().unwrap(), 10);
-            assert_eq!(l.pop_front().unwrap(), 20);
-            assert_eq!(l.pop_front().unwrap(), 30);
-            assert_eq!(l.pop_front().unwrap(), 40);
-            assert_eq!(l.pop_front().unwrap(), 50);
-            assert_eq!(l.pop_front().unwrap(), 23);
-
-            fill_list(&mut l);
-            let mut elem = super::Element::new(23);
-            let tmp = l.last;
-            l.last = &mut elem;
-            unsafe {
-                (*tmp).insert_after(Box::new(elem));
-            }
-            assert_eq!(l.pop_back().unwrap(), 23);
-            assert_eq!(l.pop_back().unwrap(), 50);
-            assert_eq!(l.pop_back().unwrap(), 40);
-            assert_eq!(l.pop_back().unwrap(), 30);
-            assert_eq!(l.pop_back().unwrap(), 20);
-            assert_eq!(l.pop_back().unwrap(), 10);
-            assert_eq!(l.pop_back().unwrap(), -20);
-         
-
-            fill_list(&mut l);
-            let mut elem = super::Element::new(23);
-            let tmp = l.last;
-            l.last = &mut elem;
-            unsafe {
-                (*tmp).insert_after(Box::new(elem));
-            }
-            assert_eq!(l.pop_back().unwrap(), 23);
-            assert_eq!(l.pop_front().unwrap(), -20);
-            assert_eq!(l.pop_front().unwrap(), 10);
-            assert_eq!(l.pop_front().unwrap(), 20);
-            assert_eq!(l.pop_front().unwrap(), 30);
-            assert_eq!(l.pop_front().unwrap(), 40);
-            assert_eq!(l.pop_front().unwrap(), 50);  
-        }
-
+        self.len -= 1;
     }
 }
-use self::internal::tests;
-    fn main() {
-        //tests::test_insert_before_first();
+
+impl<T> Element<T> {
+    fn new(elem: T) -> Self {
+        Element {
+            next: None,
+            prev: ptr::null_mut(),
+            elem,
+        }
     }
+
+    /* Hinter dem Element (self) wird das Element elem in die Liste eingefügt. Hier bei
+        muss beachtet werden, dass extern die Size angepasst werden muss und der Last-Zeiger evtl. angepasst werden muss. */
+    fn insert_after(&mut self, mut elem: Box<Element<T>>) {
+        elem.prev = &mut *self;
+        self.next = Some(elem);
+    }
+
+    /* Vor dem Element (self) wird das Element elem in die Liste eingefügt. Hier bei
+        muss beachtet werden, dass extern die Size angepasst werden muss und ggf. der First-Zeiger angepasst werden muss.  */
+    fn insert_before(mut self, elem: *mut Element<T>) {
+        unsafe {
+            self.prev = &mut *elem;
+            (*elem).next = Some(Box::new(self));
+        }
+    }
+}
+
+// #[cfg(test)]
+pub mod tests {
+    #[test]
+    pub fn test_insert_and_pop() {
+        let mut l = super::List::new();
+        fill_list(&mut l);
+        assert_eq!(l.len(), 6);
+
+        let len = l.len();
+        assert_eq!(l.pop_front().unwrap(), -20);
+        assert_eq!(l.pop_front().unwrap(), 10);
+        assert_eq!(l.pop_front().unwrap(), 20);
+        assert_eq!(l.pop_front().unwrap(), 30);
+        assert_eq!(l.pop_front().unwrap(), 40);
+        assert_eq!(l.pop_front().unwrap(), 50);
+
+        fill_list(&mut l);
+        
+        assert_eq!(l.pop_back().unwrap(), 50);
+        assert_eq!(l.pop_back().unwrap(), 40);
+        assert_eq!(l.pop_back().unwrap(), 30);
+        assert_eq!(l.pop_back().unwrap(), 20);
+        assert_eq!(l.pop_back().unwrap(), 10);
+        assert_eq!(l.pop_back().unwrap(), 0);
+
+        fill_list(&mut l);
+
+        assert_eq!(l.pop_front().unwrap(), -20);
+        assert_eq!(l.pop_back().unwrap(), 50);
+        assert_eq!(l.pop_front().unwrap(), 10);
+        assert_eq!(l.pop_back().unwrap(), 40);
+        assert_eq!(l.pop_front().unwrap(), 20);
+        assert_eq!(l.pop_back().unwrap(), 30);
+    }
+
+    #[inline]
+    fn fill_list(l: &mut super::List<i32>) {
+        l.insert_at_end(-20);
+        l.insert_at_end(10);
+        l.insert_at_end(20);
+        l.insert_at_end(30);
+        l.insert_at_end(40);
+        l.insert_at_end(50);
+    }
+
+    #[test]
+    pub fn test_insert_before_first() {
+        let mut l = super::List::new();
+        fill_list(&mut l);
+        let mut elem = Box::new(super::Element::new(23));
+        l.first.unwrap().insert_before(&mut *elem);
+        // Das kann die Methode aus technischen Gründen nicht übernehmen und muss extern gemacht werden!
+        l.first = Some(elem);
+
+        assert_eq!(l.pop_front().unwrap(), 23);
+        assert_eq!(l.pop_front().unwrap(), -20);
+        assert_eq!(l.pop_front().unwrap(), 10);
+        assert_eq!(l.pop_front().unwrap(), 20);
+        assert_eq!(l.pop_front().unwrap(), 30);
+        assert_eq!(l.pop_front().unwrap(), 40);
+        assert_eq!(l.pop_front().unwrap(), 50);
+
+        fill_list(&mut l);
+        let mut elem = Box::new(super::Element::new(23));
+        l.first.unwrap().insert_before(&mut *elem);
+        // Das kann die Methode aus technischen Gründen nicht übernehmen und muss extern gemacht werden!
+        l.first = Some(elem);
+        assert_eq!(l.pop_back().unwrap(), 50);
+        assert_eq!(l.pop_back().unwrap(), 40);
+        assert_eq!(l.pop_back().unwrap(), 30);
+        assert_eq!(l.pop_back().unwrap(), 20);
+        assert_eq!(l.pop_back().unwrap(), 10);
+        assert_eq!(l.pop_back().unwrap(), -20);
+        assert_eq!(l.pop_back().unwrap(), 23);
+
+        fill_list(&mut l);
+        let mut elem = Box::new(super::Element::new(23));
+        l.first.unwrap().insert_before(&mut *elem);
+        // Das kann die Methode aus technischen Gründen nicht übernehmen und muss extern gemacht werden!
+        l.first = Some(elem);
+        assert_eq!(l.pop_front().unwrap(), 23);
+        assert_eq!(l.pop_back().unwrap(), 50);
+        assert_eq!(l.pop_back().unwrap(), 40);
+        assert_eq!(l.pop_back().unwrap(), 30);
+        assert_eq!(l.pop_back().unwrap(), 20);
+        assert_eq!(l.pop_back().unwrap(), 10);
+        assert_eq!(l.pop_back().unwrap(), -20);
+        
+        
+    }
+
+    #[test]
+    pub fn test_insert_after_last() {
+        let mut l = super::List::new();
+        fill_list(&mut l);
+        let mut elem = super::Element::new(23);
+        let tmp = l.last;
+        l.last = &mut elem;
+        unsafe {
+            (*tmp).insert_after(Box::new(elem));
+        }
+
+        // Das kann die Methode aus technischen Gründen nicht übernehmen und muss extern gemacht werden!
+
+        assert_eq!(l.pop_front().unwrap(), -20);
+        assert_eq!(l.pop_front().unwrap(), 10);
+        assert_eq!(l.pop_front().unwrap(), 20);
+        assert_eq!(l.pop_front().unwrap(), 30);
+        assert_eq!(l.pop_front().unwrap(), 40);
+        assert_eq!(l.pop_front().unwrap(), 50);
+        assert_eq!(l.pop_front().unwrap(), 23);
+
+        fill_list(&mut l);
+        let mut elem = super::Element::new(23);
+        let tmp = l.last;
+        l.last = &mut elem;
+        unsafe {
+            (*tmp).insert_after(Box::new(elem));
+        }
+        assert_eq!(l.pop_back().unwrap(), 23);
+        assert_eq!(l.pop_back().unwrap(), 50);
+        assert_eq!(l.pop_back().unwrap(), 40);
+        assert_eq!(l.pop_back().unwrap(), 30);
+        assert_eq!(l.pop_back().unwrap(), 20);
+        assert_eq!(l.pop_back().unwrap(), 10);
+        assert_eq!(l.pop_back().unwrap(), -20);
+        
+
+        fill_list(&mut l);
+        let mut elem = super::Element::new(23);
+        let tmp = l.last;
+        l.last = &mut elem;
+        unsafe {
+            (*tmp).insert_after(Box::new(elem));
+        }
+        assert_eq!(l.pop_back().unwrap(), 23);
+        assert_eq!(l.pop_front().unwrap(), -20);
+        assert_eq!(l.pop_front().unwrap(), 10);
+        assert_eq!(l.pop_front().unwrap(), 20);
+        assert_eq!(l.pop_front().unwrap(), 30);
+        assert_eq!(l.pop_front().unwrap(), 40);
+        assert_eq!(l.pop_front().unwrap(), 50);  
+    }
+
+}
--- Implementierung/src/lib.rs
+++ Implementierung/src/lib.rs
@@ -1,15 +1,17 @@
-use std::collections::LinkedList;
 use std::mem;
 
+mod internal;
+use internal::{List,Element};
 use fnv::FnvHashMap;
 
 pub trait PredecessorList<T> {
-    fn insert(&self,element: T);
-    fn delete(&self,element: T);
+    fn insert(&mut self,element: T);
+    fn delete(&mut self,element: T);
     fn predecessor(&self,number: T) -> Option<T>;
     fn sucessor(&self,number: T) -> Option<T>;
     fn minimum(&self) -> Option<T>;
     fn maximum(&self) -> Option<T>; 
+    fn contains(&self) -> bool;
 }
 
 pub type Store = i32;
@@ -18,10 +20,11 @@ pub struct STree<Store> {
     pub maximum: Store,
     pub count: u32,
     pub root_table: [FnvHashMap<u8,FnvHashMap<u8,u32>>;1 << (8 * mem::size_of::<i32>()/2)],
-    pub root_top: [u8; 1 << (8 * mem::size_of::<i32>()/2)/8],
-    pub l1_top: [u8; (1 << (8 * mem::size_of::<i32>()/2))/32/8],
-    pub l2_top: [u8; ((1 << (8 * mem::size_of::<i32>()/2))/32)/32/8],
-    pub element_list: LinkedList<Store>,
+    // Uff was und warum passiert hier das?
+    pub root_top: [u32; 1 << (8 * mem::size_of::<i32>()/2)/32],
+    pub l1_top: [u32; (1 << (8 * mem::size_of::<i32>()/2))/32/8],
+    pub l2_top: [u32; ((1 << (8 * mem::size_of::<i32>()/2))/32)/32/8],
+    pub element_list: internal::List<Store>,
 }
 
 /* TODO :
@@ -29,6 +32,6 @@ pub struct STree<Store> {
 - prüfen ob maximum und minimum Methoden ohne Variablen und mit element_list.first, element_list.last nicht schneller sind
 - prüfen wie die Datenstruktur im Vergleich zur Datenstruktur in C++ abschneidet
 - prüfen ob die 2Mbyte Initialisierungsspeicher durch Verzicht auf Pointer, einen Leistungsschub bringen 
-- herausfinden ob X86 CPUs 32-Bit oder 64-Bit Instruktionen zum Ermitteln von bestimmten Bits haben
+- herausfinden ob X86 CPUs 32-Bit oder 64-Bit Instruktionen zum Ermitteln von bestimmten Bits haben -> leading Zeros ext.
 - u32 Bit je Zelle in root_top (weniger Top Arrays evtl. in der Praxis notwendig!)
 */
