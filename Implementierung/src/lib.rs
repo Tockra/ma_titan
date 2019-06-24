65ed3f939b14be248fb1f9b438b07aea593c559f
Tim <tim.tannert@tu-dortmund.de>
2019-06-24 15:37:27 +0200

    - Level Struct für die HashMap-Level hinzugefügt (wegen der Minima und Maxima - STree struct angepasst - new-methoden geschrieben

--- Implementierung/src/lib.rs
+++ Implementierung/src/lib.rs
@@ -16,15 +16,47 @@ pub trait PredecessorList<T> {
 
 pub type Store = i32;
 pub struct STree<Store> {
-    pub minimum: Store,
-    pub maximum: Store,
-    pub count: u32,
-    pub root_table: [FnvHashMap<u8,FnvHashMap<u8,u32>>;1 << (8 * mem::size_of::<i32>()/2)],
-    // Uff was und warum passiert hier das?
+    pub root_table: [Level<Level<Store>>;1 << (8 * mem::size_of::<i32>()/2)],
+    // Da die Größe in in Bytes von size_of zurückgegeben wird, mal 8. Durch 32 wegen der Fenstergröße
     pub root_top: [u32; 1 << (8 * mem::size_of::<i32>()/2)/32],
-    pub l1_top: [u32; (1 << (8 * mem::size_of::<i32>()/2))/32/8],
-    pub l2_top: [u32; ((1 << (8 * mem::size_of::<i32>()/2))/32)/32/8],
+    pub l1_top: [u32; (1 << (8 * mem::size_of::<i32>()/2))/32/32],
+    pub l2_top: [u32; ((1 << (8 * mem::size_of::<i32>()/2))/32)/32/32],
     pub element_list: internal::List<Store>,
+}
+
+struct Level<T,Store> {
+    pub hash_map: FnvHashMap<u8,T>,
+    pub maximum: Option<Store>,
+    pub minimum: Option<Store>,
+}
+
+impl Level<T,Store> {
+    fn new() -> Level<T,Store> {
+        Level {
+            hash_map: FnvHashMap<u8,T>::default(),
+            maximum: None,
+            minimum: None,
+        }
+    }
+}
+
+impl PredecessorList<Store> for STree<Store> {
+    pub fn new() -> STree<Store> {
+        STree {
+            element_list: List::new(),
+            root_top: [0; 1 << (8 * mem::size_of::<i32>()/2)/32],
+            l1_top: [0; (1 << (8 * mem::size_of::<i32>()/2))/32/32],
+            l2_top: [0; ((1 << (8 * mem::size_of::<i32>()/2))/32)/32/32],
+            root_table: [Level<Level<i32>>::new();(8 * mem::size_of::<i32>()/2)]
+        }
+    }
+    pub fn insert(&mut self,element: T);
+    pub fn delete(&mut self,element: T);
+    pub fn predecessor(&self,number: T) -> Option<T>;
+    pub fn sucessor(&self,number: T) -> Option<T>;
+    pub fn minimum(&self) -> Option<T>;
+    pub fn maximum(&self) -> Option<T>; 
+    pub fn contains(&self) -> bool;
 }
 
 /* TODO :
