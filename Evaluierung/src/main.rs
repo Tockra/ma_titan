07c81e031aa88bb60421a8d3a15003b351857a45
Tim <tim.tannert@tu-dortmund.de>
2019-08-20 17:29:59 +0200

    Datengenerierung

--- Evaluierung/src/main.rs
+++ Evaluierung/src/main.rs
@@ -12,7 +12,7 @@ use uint::u40;
 const SEED: u128 = 0xcafef00dd15ea5e5;
 fn main() {
     let two: u64 = 2;
-    for i in 0..3 {
+    for i in 0..41 {
         generate_values(two.pow(i) as usize);
     }
 }
@@ -26,10 +26,10 @@ fn generate_values(n: usize) {
         while values.contains(&u40::from(x)) {
             x = state.gen_range(0, u64::from(u40::max_value()));
         }
-        //values.push(u40::from(x));
-        println!("Random: {}",x);
+        values.push(u40::from(x));
+        //println!("Random: {}",x);
     }
-   // write_to_file(format!("testdata/u40/{}.data", n), &values);
+    write_to_file(format!("testdata/u40/{}.data", n), &values);
 }
 
 /// Serializiert den übergebenen Vector und schreibt diesen in eine Datei namens `name`.
