1c2374b1bc9753b8026db0f6362fd4d3a19155ff
Tim <tim.tannert@tu-dortmund.de>
2019-08-21 20:41:18 +0200

    Datengenerator optimiert

--- eval_data_generator/src/main.rs
+++ eval_data_generator/src/main.rs
@@ -21,31 +21,17 @@ fn generate_test_data<T: Typable + Into<u64>>(exponent: u64) {
     let mut state = Mcg128Xsl64::new(SEED);
     let max_value = (1u64<<exponent) as usize;
     let mut result: Vec<u64> = (0u64..(T::max_value()).into()).choose_multiple(&mut state, max_value);
-    let mut clone = result.clone();
-    clone.sort();
-    write_to_file(format!("../testdata/{}/2^{}.data", T::TYPE,exponent),&clone);
-    for i in (0..exponent).rev() {
-        let cut = result.len() - (max_value - (1<<i) as usize); 
-        result = result.split_off(cut);
-        let mut clone = result.clone();
-        clone.sort();
-        write_to_file(format!("../testdata/{}/2^{}.data",T::TYPE, i),&clone);
-    }
-}
 
-/// Generiert `n` Normalverteilte Werte im u40 BereichDateien und speichert diese in der Datei "`n`.data"
-/*fn generate_values(n: usize) {
-    let mut values = vec![];
-    let mut state = Mcg128Xsl64::new(SEED);
-    for _ in 0..n {
-        let mut x = state.gen_range(0, u64::from(u40::max_value()));
-        while values.contains(&u40::from(x)) {
-            x = state.gen_range(0, u64::from(u40::max_value()));
-        }
-        values.push(u40::from(x));
+    for i in 0..exponent {
+        let cut = result.len() - (max_value - (1<<i) as usize); 
+        let (result, _) = result.split_at_mut(cut);
+        result.sort();
+        write_to_file(format!("../testdata/{}/2^{}.data",T::TYPE, i),&result.to_vec());
     }
-    write_to_file(format!("testdata/u40/{}.data", n), &values);
-}*/
+
+    result.sort();
+    write_to_file(format!("../testdata/{}/2^{}.data",T::TYPE, exponent),&result);
+}
 
 /// Serializiert den übergebenen Vector und schreibt diesen in eine Datei namens `name`.
 fn write_to_file(name: String, val: &Vec<u64>) {
