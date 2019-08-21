d5540f51222b8dddac83cec30725117aa6a5cb56
Tim <tim.tannert@tu-dortmund.de>
2019-08-21 21:14:36 +0200

    Refactoring

--- Implementierung/benches/static.rs
+++ Implementierung/benches/static.rs
@@ -22,7 +22,7 @@ use std::fmt::Debug;
 
 use predecessor_list::help::internal::PredecessorSetStatic;
 use predecessor_list::data_structures::statics::STree;
-use predecessor_list::data_structures::binary::BinarySearch;
+use self::bench_data::BinarySearch;
 
 use uint::u40;
 use uint::Typable;
@@ -89,3 +89,104 @@ criterion_group!(binary_search_instr, pred_and_succ_benchmark<u40,BinarySearch>)
 
 
 criterion_main!(stree_gen, binary_search_gen, stree_instr, binary_search_instr);
+
+
+mod bench_data {
+    use uint::u40;
+    use predecessor_list::help::internal::{PredecessorSetStatic};
+
+    type Int = u40;
+    pub struct BinarySearch {
+        element_list: Box<[Int]>
+    }
+
+    impl PredecessorSetStatic<Int> for BinarySearch {
+        fn new(elements: Vec<Int>) -> Self {
+            Self {
+                element_list: elements.into_boxed_slice(),
+            }
+        }
+
+        fn predecessor(&self,number: Int) -> Option<Int> {
+            if self.element_list.len() == 0 {
+                None
+            } else {
+                self.pred(number, 0, self.element_list.len()-1)
+            }
+        }
+
+        fn sucessor(&self,number: Int) -> Option<Int>{
+            if self.element_list.len() == 0 {
+                None
+            } else {
+                self.succ(number, 0, self.element_list.len()-1)
+            }
+        }
+        
+        fn minimum(&self) -> Option<Int>{
+            if self.element_list.len() == 0 {
+                None
+            } else {
+                Some(self.element_list[0])
+            }
+        }
+
+        fn maximum(&self) -> Option<Int>{
+            if self.element_list.len() == 0 {
+                None
+            } else {
+                Some(self.element_list[self.element_list.len()-1])
+            }
+        }
+
+        fn contains(&self, number: Int) -> bool {
+            self.element_list.contains(&number)
+        }
+
+        const TYPE: &'static str = "BinarySearch";
+    }
+
+    impl BinarySearch {
+        fn succ(&self, element: Int, l: usize, r: usize) -> Option<Int> {
+            let mut l = l;
+            let mut r = r;
+
+            while r != l {
+                let m = (l+r)/2;
+                if self.element_list[m] > element {
+                    r = m;
+                } else {
+                    l = m+1;
+                }
+            }
+            if self.element_list[l] >= element {
+                Some(self.element_list[l])
+            } else {
+                None
+            }
+        }
+
+        fn pred(&self, element: Int, l: usize, r: usize) -> Option<Int> {
+            let mut l = l;
+            let mut r = r;
+
+            while l != r {
+                let m = (l+r)/2;
+                if self.element_list[m] < element {
+                    r = m
+                } else {
+                    l = m+1;
+                }
+            }
+    
+            if element >= self.element_list[l] {
+                Some(self.element_list[l])
+            } else {
+                None
+            }
+        }
+
+
+    }
+
+}
