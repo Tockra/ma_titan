4c9b5bfe2bcf334e1bec3a16fe0ae3d46309da03
Tim <tim.tannert@tu-dortmund.de>
2019-09-17 16:43:32 +0200

    BinarySearch verschoben, generisch gemacht

--- benches/stree_immutable.rs
+++ benches/stree_immutable.rs
@@ -28,8 +28,7 @@ use std::time::Duration;
 use std::fs::OpenOptions;
 
 use ma_titan::internal::PredecessorSetStatic;
-use ma_titan::default::immutable::STree;
-use self::bench_data::BinarySearch;
+use ma_titan::default::immutable::{STree, BinarySearch};
 use uint::u40;
 use uint::Typable;
 const REPEATS: usize = 10_000;
@@ -150,11 +149,11 @@ pub fn cache_clear() {
 
 
 criterion_group!(stree_gen_u40, static_build_benchmark<u40,STree<u40>>);
-criterion_group!(binary_search_gen_u40, static_build_benchmark<u40,BinarySearch>);
+criterion_group!(binary_search_gen_u40, static_build_benchmark<u40,BinarySearch<u40>>);
 criterion_group!(stree_pred_u40, pred_benchmark<u40,STree<u40>>);
-criterion_group!(binary_search_pred_u40, pred_benchmark<u40,BinarySearch>);
+criterion_group!(binary_search_pred_u40, pred_benchmark<u40,BinarySearch<u40>>);
 criterion_group!(stree_succ_u40, succ_benchmark<u40,STree<u40>>);
-criterion_group!(binary_search_succ_u40, succ_benchmark<u40,BinarySearch>);
+criterion_group!(binary_search_succ_u40, succ_benchmark<u40,BinarySearch<u40>>);
 
 // Tmp: stree_gen_u40, binary_search_gen_u40, 
 criterion_main!(stree_pred_u40, binary_search_pred_u40,stree_succ_u40, binary_search_succ_u40, generate_sql_plot_input);
@@ -186,108 +185,4 @@ fn generate_sql_plot_input() {
 
         } 
     }
-}
-
-
-mod bench_data {
-    use uint::u40;
-    use ma_titan::internal::PredecessorSetStatic;
-
-    // Todo Generics
-    type Int = u40;
-
-    #[derive(Clone)]
-    pub struct BinarySearch {
-        element_list: Box<[Int]>
-    }
-
-    impl PredecessorSetStatic<Int> for BinarySearch {
-        fn new(elements: Vec<Int>) -> Self {
-            Self {
-                element_list: elements.into_boxed_slice(),
-            }
-        }
-
-        fn predecessor(&self,number: Int) -> Option<Int> {
-            if self.element_list.len() == 0 {
-                None
-            } else {
-                self.pred(number, 0, self.element_list.len()-1)
-            }
-        }
-
-        fn successor(&self,number: Int) -> Option<Int>{
-            if self.element_list.len() == 0 {
-                None
-            } else {
-                self.succ(number, 0, self.element_list.len()-1)
-            }
-        }
-        
-        fn minimum(&self) -> Option<Int>{
-            if self.element_list.len() == 0 {
-                None
-            } else {
-                Some(self.element_list[0])
-            }
-        }
-
-        fn maximum(&self) -> Option<Int>{
-            if self.element_list.len() == 0 {
-                None
-            } else {
-                Some(self.element_list[self.element_list.len()-1])
-            }
-        }
-
-        fn contains(&self, number: Int) -> bool {
-            self.element_list.contains(&number)
-        }
-
-        const TYPE: &'static str = "BinarySearch";
-    }
-
-    impl BinarySearch {
-        fn succ(&self, element: Int, l: usize, r: usize) -> Option<Int> {
-            let mut l = l;
-            let mut r = r;
-
-            while r != l {
-                let m = (l+r)/2;
-                if self.element_list[m] > element {
-                    r = m;
-                } else {
-                    l = m+1;
-                }
-            }
-            if self.element_list[l] >= element {
-                Some(self.element_list[l])
-            } else {
-                None
-            }
-        }
-
-        fn pred(&self, element: Int, l: usize, r: usize) -> Option<Int> {
-            let mut l = l;
-            let mut r = r;
-
-            while l != r {
-                let m = (l+r)/2;
-                if self.element_list[m] < element {
-                    r = m
-                } else {
-                    l = m+1;
-                }
-            }
-    
-            if element >= self.element_list[l] {
-                Some(self.element_list[l])
-            } else {
-                None
-            }
-        }
-
-
-    }
-
 }
