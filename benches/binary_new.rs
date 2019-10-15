d0b217c07cf2540dd7d7b932fee4f55f0857ace0
Tim <tim.tannert@tu-dortmund.de>
2019-10-16 00:15:59 +0200

    Laufzeitmessungen ausgegliedert

--- benches/binary_new.rs
+++
@@ -1,7 +0,0 @@
-use criterion::criterion_main;
-
-use ma_titan::benches::static_build_benchmark;
-use ma_titan::benches::BinarySearch;
-use uint::u40;
-
-criterion_main!(static_build_benchmark<u40,BinarySearch<u40>>);
