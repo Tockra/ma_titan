19db837b7b9076235f716158253ed3bd0ac5a4fb
Tim <tim.tannert@tu-dortmund.de>
2019-10-21 20:51:48 +0200

    Threshold auf 378 gesenkt

--- src/internal.rs
+++ src/internal.rs
@@ -573,7 +573,7 @@ impl<K:'static + Clone,T:'static + Clone> Clone for MphfHashMapThres<K,T> {
 impl<K:'static + Eq + std::fmt::Display + std::marker::Send + std::marker::Sync + std::hash::Hash + std::fmt::Debug + Into<u16> + Ord + Copy + std::hash::Hash,T: 'static> MphfHashMapThres<K,T> {
     pub fn new(keys: &Vec<K>, objects: Box<[T]>) -> Self {
         LEVEL_COUNT.fetch_add(1, Ordering::SeqCst);
-        if keys.len() <= 512 {
+        if keys.len() <= 378 {
             let mut values = Vec::with_capacity(keys.len());
             
             let objects = objects.into_vec();
