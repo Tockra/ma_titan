28e5f895d358fbda376473114b160a00e674f18f
Tim <tim.tannert@tu-dortmund.de>
2019-10-22 16:49:33 +0200

    Update File ergänzt

--- update-branches.sh
+++ update-branches.sh
@@ -4,13 +4,10 @@ git pull &&
 git checkout space_efficient_space &&
 git merge --no-edit space_efficient &&
 git push &&
-git checkout fnv_hash_space && 
-git merge --no-edit fnv_hash &&
+git checkout space_efficient_max_space &&
+git merge --no-edit space_efficient_space_max &&
 git push &&
-git checkout rustc_hash_hash_space && 
-git merge --no-edit rustc_hash_hash &&
-git push &&
-git checkout hash_brown_hash_space && 
-git merge --no-edit hash_brown_hash &&
+git checkout space_efficient_128_space &&
+git merge --no-edit space_efficient_128 &&
 git push 
  
