f0cafa73302b85b570c0a5ad5ef1c831d9ce30dc
Tim <tim.tannert@tu-dortmund.de>
2019-10-23 20:09:27 +0200

    num = "0.2.0"

--- src/default/immutable.rs
+++ src/default/immutable.rs
@@ -831,6 +831,7 @@ mod tests {
         }
     }
 
+    use num::Bounded;
      /// # Äquivalenzklassentest mit Bruteforce
     /// `locate_or_pred` wird getestet. Dabei werden in jeder Ebene die gesuchten Elemente einmal im Minimum, im Maximum und irgendwo dazwischen liegen.
     #[test]
