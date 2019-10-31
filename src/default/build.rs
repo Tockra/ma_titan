cd879ed45215c15db254a001e6515be56bf3d08b
Tim <tim.tannert@tu-dortmund.de>
2019-10-31 16:54:39 +0100

    Dynlookup -> 16 bit

--- src/default/build.rs
+++ src/default/build.rs
@@ -2,7 +2,7 @@ use crate::default::immutable::Int;
 use crate::default::immutable::{Level,L3Ebene};
 use crate::internal::{Splittable,PointerEnum};
 #[inline]
-pub fn insert_l3_level<T: Int + Into<u64>>(l3_level: &mut L3Ebene<T>,index: usize, k: u8, elements: &[T]) {
+pub fn insert_l3_level<T: Int + Into<u64>>(l3_level: &mut L3Ebene<T>,index: usize, k: u16, elements: &[T]) {
     if l3_level.is_null() {
         *l3_level = L3Ebene::from_usize(Box::new(index));
     } else {
