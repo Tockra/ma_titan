6434163a001184a7e72e9485cf0f834dec0d1505
Tim <tim.tannert@tu-dortmund.de>
2019-11-13 04:37:13 +0100

    Test Direct Pointers to array
    changed ?
    Fixed Array Drop
    Fixed bug

--- src/default/build.rs
+++ src/default/build.rs
@@ -2,9 +2,9 @@ use crate::default::immutable::Int;
 use crate::default::immutable::{Level,L3Ebene};
 use crate::internal::{Splittable,PointerEnum};
 #[inline]
-pub fn insert_l3_level<T: Int + Into<u64>>(l3_level: &mut L3Ebene<T>,index: usize, k: u16, elements: &[T]) {
+pub fn insert_l3_level<T: Int + Into<u64>>(l3_level: &mut L3Ebene<T>,elem: &T, k: u16, elements: &[T]) {
     if l3_level.is_null() {
-        *l3_level = L3Ebene::from_usize(Box::new(index));
+        *l3_level = L3Ebene::from_usize(elem as *const T);
     } else {
         match l3_level.get() {
             PointerEnum::First(l3_level) => {
@@ -13,23 +13,23 @@ pub fn insert_l3_level<T: Int + Into<u64>>(l3_level: &mut L3Ebene<T>,index: usiz
                 l3_level.lx_top.set_bit(k as usize);
             
                 //Maximasetzung auf der zweiten Ebene
-                l3_level.maximum = index;
+                l3_level.maximum = elem as *const T;
 
-                l3_level.hash_map.insert(k, index);
+                l3_level.hash_map.insert(k, elem as *const T);
             },
 
             PointerEnum::Second(e) => {
-                let (_,_,k2) = Splittable::split_integer_down(&elements[*e]);
+                let (_,_,k2) = Splittable::split_integer_down(e);
                 let mut l3_level_n = Level::new();
 
                 debug_assert!(k2!=k);
 
                     // Minima- und Maximasetzung auf der zweiten Ebene
-                l3_level_n.minimum = *e;
-                l3_level_n.maximum = index;
+                l3_level_n.minimum = e as *const T;
+                l3_level_n.maximum = elem  as *const T;
 
-                l3_level_n.hash_map.insert(k2, *e);
-                l3_level_n.hash_map.insert(k, index);
+                l3_level_n.hash_map.insert(k2, e as *const T);
+                l3_level_n.hash_map.insert(k, elem as *const T);
                 l3_level_n.lx_top.set_bit(k as usize);
                 l3_level_n.lx_top.set_bit(k2 as usize);
             
